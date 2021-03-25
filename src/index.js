d0a34c3a3a42a76ad85c26600ba3c520434bdf71
sitek94 <msitkowski94@gmail.com>
2021-03-25 15:11:21 +0100

    fix(ant-design): remove StrictMode to silence the error

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import 'antd/dist/antd.css'
 
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
