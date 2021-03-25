40798ea9e5467bb7882475c088ecaf860e6c87c8
sitek94 <msitkowski94@gmail.com>
2021-03-25 13:21:40 +0100

    feat(marker): improve marker styles

--- src/components/marker/index.js
+++ src/components/marker/index.js
@@ -6,15 +6,22 @@ import theme from 'theme'
 import getIconFromString from './get-icon-from-string'
 
 const Circle = styled.div`
-  position: absolute;
-  transform: translate(-50%, -50%);
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 30px;
-  border-radius: 50%;
+  cursor: pointer;
   color: ${theme.colors.sunset_orange};
+  position: absolute;
+  transform: scale(0.95) translate(-50%, -50%);
+  transform-origin: 0 0;
+  transition: all 0.2s ease-in;
   opacity: 0.7;
+
+  &:hover {
+    transform: scale(1) translate(-50%, -50%);
+    opacity: 1;
+  }
 `
 
 export default function Marker({ title }) {
