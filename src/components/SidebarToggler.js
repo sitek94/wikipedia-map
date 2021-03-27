874ea1fcb3705a1de329f390a1626b3611dc5c15
sitek94 <msitkowski94@gmail.com>
2021-03-27 20:09:11 +0100

    feat(sidebar): make the toggler theme-aware

--- src/components/SidebarToggler.js
+++ src/components/SidebarToggler.js
@@ -1,11 +1,31 @@
-import { Button } from 'antd'
+import styled from 'styled-components'
+import { Button as AntButton } from 'antd'
 import { MenuOutlined } from '@ant-design/icons'
 
 import { useMapStore } from 'pages/map/store'
+import theme from 'theme'
 
 export default function SidebarToggler() {
   const [, { setIsSidebarVisible }] = useMapStore()
   const openSidebar = () => setIsSidebarVisible(true)
 
-  return <Button type="primary" onClick={openSidebar} icon={<MenuOutlined />} />
+  return (
+    <Button
+      size="large"
+      shape="circle"
+      type="text"
+      onClick={openSidebar}
+      icon={<MenuOutlined style={{ fontSize: 22 }} />}
+    />
+  )
 }
+
+const Button = styled(AntButton)`
+  color: ${theme.colors.text.default};
+
+  &.ant-btn-text:hover,
+  &.ant-btn-text:focus {
+    color: ${theme.colors.text.default};
+    background-color: ${theme.colors.background.level2};
+  }
+`
