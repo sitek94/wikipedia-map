783b4fe4f32c59af9fa3a7ac2506a9566c2217ef
sitek94 <msitkowski94@gmail.com>
2021-03-29 15:26:03 +0200

    feat(saved-articles-list): notify the the user if there are no saved articles

--- src/pages/map/components/SavedArticlesList.js
+++ src/pages/map/components/SavedArticlesList.js
@@ -6,17 +6,50 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import IconButton from '@material-ui/core/IconButton'
 import LocationIcon from '@material-ui/icons/LocationSearching'
 import HeartIcon from '@material-ui/icons/Favorite'
+import Tooltip from '@material-ui/core/Tooltip'
 import { red } from '@material-ui/core/colors'
 
 import { emit } from '../mediator'
 import { useMapStore } from '../store'
 import CollapsibleListItem from 'components/CollapsibleListItem'
-import { Tooltip } from '@material-ui/core'
 
 export default function SavedArticlesList() {
   const [{ savedArticles }] = useMapStore()
 
   const locationLabel = 'Go to article location'
+
+  const articlesList = (
+    <List component="div" disablePadding dense>
+      {savedArticles.map(({ pageid, title, lat, lng }) => (
+        <ListItem
+          key={pageid}
+          onClick={() => emit('sidebarSavedArticleClick', { lat, lng })}
+          button
+        >
+          <ListItemText primary={title} />
+          <ListItemSecondaryAction>
+            <Tooltip title={locationLabel} placement="right">
+              <IconButton
+                edge="end"
+                aria-label={locationLabel}
+                onClick={() =>
+                  emit('savedArticleLocationClicked', { lat, lng })
+                }
+              >
+                <LocationIcon />
+              </IconButton>
+            </Tooltip>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))}
+    </List>
+  )
+
+  const emptyListMessage = (
+    <ListItem>
+      <ListItemText primary="There are no saved articles" />
+    </ListItem>
+  )
 
   return (
     <CollapsibleListItem
@@ -24,30 +57,7 @@ export default function SavedArticlesList() {
       icon={<HeartIcon style={{ color: red[500] }} />}
       onExpand={() => emit('savedArticlesExpanded')}
     >
-      <List component="div" disablePadding dense>
-        {savedArticles.map(({ pageid, title, lat, lng }) => (
-          <ListItem
-            key={pageid}
-            onClick={() => emit('sidebarSavedArticleClick', { lat, lng })}
-            button
-          >
-            <ListItemText primary={title} />
-            <ListItemSecondaryAction>
-              <Tooltip title={locationLabel} placement="right">
-                <IconButton
-                  edge="end"
-                  aria-label={locationLabel}
-                  onClick={() =>
-                    emit('savedArticleLocationClicked', { lat, lng })
-                  }
-                >
-                  <LocationIcon />
-                </IconButton>
-              </Tooltip>
-            </ListItemSecondaryAction>
-          </ListItem>
-        ))}
-      </List>
+      {savedArticles.length ? articlesList : emptyListMessage}
     </CollapsibleListItem>
   )
 }
