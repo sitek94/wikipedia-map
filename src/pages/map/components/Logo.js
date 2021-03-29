ebb8a51af49cd8b5fa075ad9cd1d2fae3fc35ce6
sitek94 <msitkowski94@gmail.com>
2021-03-29 14:00:43 +0200

    feat(logo): create Logo component

---
+++ src/pages/map/components/Logo.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Typography } from '@material-ui/core'
+
+export default function Logo() {
+  return (
+    <Typography variant="h6" component="h1">
+      Wikipedia Map
+    </Typography>
+  )
+}
