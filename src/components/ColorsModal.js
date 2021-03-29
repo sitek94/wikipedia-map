89f9757bbb96701719122435ab21c1af4a5e87a3
sitek94 <msitkowski94@gmail.com>
2021-03-29 19:46:05 +0200

    feat(colors-modal): add preview when changing theme colors

--- src/components/ColorsModal.js
+++ src/components/ColorsModal.js
@@ -1,11 +1,13 @@
 import * as React from 'react'
+import { useTheme } from '@material-ui/core'
+import { ColorPalette } from 'material-ui-color'
+import { FaCircle, FaLandmark, FaMonument } from 'react-icons/fa'
 import Box from '@material-ui/core/Box'
 import Dialog from '@material-ui/core/Dialog'
+import Divider from '@material-ui/core/Divider'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Typography from '@material-ui/core/Typography'
-import { useTheme } from '@material-ui/core'
-import { ColorPalette } from 'material-ui-color'
 
 import palette from 'theme/palette'
 
@@ -15,22 +17,51 @@ export default function ColorsModal({ onClose, open }) {
   return (
     <Dialog onClose={onClose} open={open}>
       <DialogTitle>Edit app colors</DialogTitle>
+
       <DialogContent dividers>
-        <Box sx={{ mb: 2 }}>
-          <Typography gutterBottom>Primary color</Typography>
-          <ColorPalette
-            palette={palette}
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <ColorSelectPane
+            color="primary"
+            title="Primary color"
             onSelect={color => setPrimaryColor(palette[color])}
           />
-        </Box>
-        <Box>
-          <Typography gutterBottom>Secondary color</Typography>
-          <ColorPalette
-            palette={palette}
+
+          <Divider flexItem orientation="vertical" />
+
+          <ColorSelectPane
+            color="secondary"
+            title="Secondary color"
             onSelect={color => setSecondaryColor(palette[color])}
           />
         </Box>
       </DialogContent>
     </Dialog>
+  )
+}
+
+function ColorSelectPane({ title, color, onSelect }) {
+  const theme = useTheme()
+
+  const iconStyle = {
+    color: theme.palette[color].main,
+    fontSize: 60,
+  }
+
+  return (
+    <Box sx={{ maxWidth: 200 }}>
+      <Typography align="center" fontWeight="bold" sx={{ mb: 1 }}>
+        {title}
+      </Typography>
+
+      <Box sx={{ mb: 3 }}>
+        <ColorPalette palette={palette} onSelect={onSelect} />
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <FaLandmark style={iconStyle} />
+        <FaMonument style={iconStyle} />
+        <FaCircle style={iconStyle} />
+      </Box>
+    </Box>
   )
 }
