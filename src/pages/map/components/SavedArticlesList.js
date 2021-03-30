c944dffc5b8731bd445ea56b29ca50f747dbb275
sitek94 <msitkowski94@gmail.com>
2021-03-30 14:47:50 +0200

    chore(saved-articles-list): remove unused code

--- src/pages/map/components/SavedArticlesList.js
+++ src/pages/map/components/SavedArticlesList.js
@@ -7,7 +7,6 @@ import IconButton from '@material-ui/core/IconButton'
 import LocationIcon from '@material-ui/icons/LocationSearching'
 import HeartIcon from '@material-ui/icons/Favorite'
 import Tooltip from '@material-ui/core/Tooltip'
-import { red } from '@material-ui/core/colors'
 
 import { emit } from '../mediator'
 import { useMapStore } from '../store'
